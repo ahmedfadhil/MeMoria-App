215e45052f449d145a645f4b16e6d7d379a535f9
ahmed fadhil <fadhil@fbk.eu>
2018-01-03 15:02:09 +0100

    created the map activity

--- app/src/main/java/com/example/android/memoria/MainActivity.java
+++ app/src/main/java/com/example/android/memoria/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.android.memoria;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
 
     @Override
@@ -21,8 +22,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<String> places = new ArrayList<String>();
         places.add("Add new places");
 
-        ArrayAdapter arrayAdapter =new ArrayAdapter(this, android.R.layout.simple_list_item_1,places);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("locationInfo",position);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/android/memoria/MapsActivity.java
+++ app/src/main/java/com/example/android/memoria/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.memoria;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +24,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Intent intent = getIntent();
+        Log.i("Location", Integer.toString(intent.getIntExtra("locationInfo", -1)));
     }
 
 
